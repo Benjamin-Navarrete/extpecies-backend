1d5f6dd507b25fc5f33ffe8a92f24d239af505b5
Gabo <gabriel.medina.s@mail.pucv.cl>
2022-10-30 11:27:49 -0300

    leer datos preguntas.json

--- src/pages/Prueba.js
+++ src/pages/Prueba.js
@@ -1,291 +1,83 @@
 import blankImage from "../assets/img/blank_image.png";
+import Preguntas from "../assets/preguntas.json"
 
-const Prueba = () => {
-  return (
+
+const Prueba =() => {
+  return(
     <div className="card">
       <div className="card-body">
         <div className="row">
           <form id="form-prueba">
-            <div className="col-12 mb-5">
-              <div className="row">
-                <div className="col-12 col-sm-3 col-md- text-center w-auto">
-                  <img src={blankImage} alt="" />
-                </div>
-                <div className="col-12 col-sm-9 align-items-center">
-                  <h3>1) Primera pregunta</h3>
-                  <p>
-                    Donec sagittis dui quis nunc semper rutrum. Etiam id ante
-                    aliquam, posuere est et, tempor libero. Lorem ipsum dolor
-                    sit amet, consectetur adipiscing elit. Nunc mi arcu,
-                    efficitur eget ligula et, luctus finibus eros. Integer et
-                    pretium lectus. Sed accumsan maximus rhoncus. In fermentum
-                    sem sapien, et placerat ex vulputate et.
-                  </p>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox1"
-                      value="option1"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox1">
-                      A) Primera alternativa
-                    </label>
+            {
+            Preguntas.map(dato =>{
+                return(
+                  <div className="col-12 mb-5">
+                    <div className="row">
+                      <div className="col-12 col-sm-3 col-md- text-center w-auto">
+                        <img src={blankImage} alt="" />
+                      </div>
+                      <div className="col-12 col-sm-9 align-items-center">
+                        <h3>{dato.pregunta}</h3>
+                        <p>{dato.enunciado}</p>
+                        <div className="form-check form-check-inline">
+                          <input
+                            className="form-check-input"
+                            type="checkbox"
+                            id="inlineCheckbox1"
+                            value="option1"
+                            name="alternativas"
+                          />
+                          <label className="form-check-label" for="inlineCheckbox1">
+                            {dato.a}
+                          </label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                          <input
+                            className="form-check-input"
+                            type="checkbox"
+                            id="inlineCheckbox2"
+                            value="option2"
+                            name="alternativas"
+                          />
+                          <label className="form-check-label" for="inlineCheckbox2">
+                            {dato.b}
+                          </label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                          <input
+                            className="form-check-input"
+                            type="checkbox"
+                            id="inlineCheckbox3"
+                            value="option3"
+                            name="alternativas"
+                          />
+                          <label className="form-check-label" for="inlineCheckbox3">
+                            {dato.c}
+                          </label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                          <input
+                            className="form-check-input"
+                            type="checkbox"
+                            id="inlineCheckbox4"
+                            value="option4"
+                            name="alternativas"
+                          />
+                          <label className="form-check-label" for="inlineCheckbox4">
+                            {dato.d}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox2"
-                      value="option2"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox2">
-                      B) Segunda alternativa
-                    </label>
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox3"
-                      value="option3"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox3">
-                      C) Tercera alternativa
-                    </label>
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox4"
-                      value="option4"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox4">
-                      D) Cuarta alternativa
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-12 mb-5">
-              <div className="row">
-                <div className="col-12 col-sm-3 col-md- text-center w-auto">
-                  <img src={blankImage} alt="" />
-                </div>
-                <div className="col-12 col-sm-9 align-items-center">
-                  <h3>2) Segunda pregunta</h3>
-                  <p>
-                    Donec sagittis dui quis nunc semper rutrum. Etiam id ante
-                    aliquam, posuere est et, tempor libero. Lorem ipsum dolor
-                    sit amet, consectetur adipiscing elit. Nunc mi arcu,
-                    efficitur eget ligula et, luctus finibus eros. Integer et
-                    pretium lectus. Sed accumsan maximus rhoncus. In fermentum
-                    sem sapien, et placerat ex vulputate et.
-                  </p>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox5"
-                      value="option5"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox5">
-                      A) Primera alternativa
-                    </label>
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox6"
-                      value="option6"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox6">
-                      B) Segunda alternativa
-                    </label>
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox7"
-                      value="option7"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox7">
-                      C) Tercera alternativa
-                    </label>
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox8"
-                      value="option8"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox8">
-                      D) Cuarta alternativa
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-12 mb-5">
-              <div className="row">
-                <div className="col-12 col-sm-3 col-md- text-center w-auto">
-                  <img src={blankImage} alt="" />
-                </div>
-                <div className="col-12 col-sm-9 align-items-center">
-                  <h3>3) Tercera pregunta</h3>
-                  <p>
-                    Donec sagittis dui quis nunc semper rutrum. Etiam id ante
-                    aliquam, posuere est et, tempor libero. Lorem ipsum dolor
-                    sit amet, consectetur adipiscing elit. Nunc mi arcu,
-                    efficitur eget ligula et, luctus finibus eros. Integer et
-                    pretium lectus. Sed accumsan maximus rhoncus. In fermentum
-                    sem sapien, et placerat ex vulputate et.
-                  </p>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox9"
-                      value="option9"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox9">
-                      A) Primera alternativa
-                    </label>
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox10"
-                      value="option10"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox10">
-                      B) Segunda alternativa
-                    </label>
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox11"
-                      value="option11"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox11">
-                      C) Tercera alternativa
-                    </label>
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox12"
-                      value="option12"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox12">
-                      D) Cuarta alternativa
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-12 mb-5">
-              <div className="row">
-                <div className="col-12 col-sm-3 col-md- text-center w-auto">
-                  <img src={blankImage} alt="" />
-                </div>
-                <div className="col-12 col-sm-9 align-items-center">
-                  <h3>4) Cuarta pregunta</h3>
-                  <p>
-                    Donec sagittis dui quis nunc semper rutrum. Etiam id ante
-                    aliquam, posuere est et, tempor libero. Lorem ipsum dolor
-                    sit amet, consectetur adipiscing elit. Nunc mi arcu,
-                    efficitur eget ligula et, luctus finibus eros. Integer et
-                    pretium lectus. Sed accumsan maximus rhoncus. In fermentum
-                    sem sapien, et placerat ex vulputate et.
-                  </p>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox13"
-                      value="option1"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox13">
-                      A) Primera alternativa
-                    </label>
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox14"
-                      value="option2"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox14">
-                      B) Segunda alternativa
-                    </label>
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox15"
-                      value="option3"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox15">
-                      C) Tercera alternativa
-                    </label>
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="inlineCheckbox16"
-                      value="option3"
-                      name="alternativas"
-                    />
-                    <label className="form-check-label" for="inlineCheckbox16">
-                      D) Cuarta alternativa
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="d-flex justify-content-center mb-3">
-              <input
-                type="submit"
-                value="Enviar"
-                className="btn btn-primary btn-block btn-lg text-white"
-              />
-            </div>
+                )
+              })
+            }
           </form>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default Prueba;
